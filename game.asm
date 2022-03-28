dbfa971b96798e5e3e95398a68686694b6de8d87
William <slicer7667@gmail.com>
2022-03-27 20:17:57 -0400

    Draw static objects/platforms
    Finished drawing static objects and platforms

--- game.asm
+++ game.asm
@@ -39,6 +39,9 @@
 .eqv	BROWN 0x795548
 .eqv	WHITE 0xffffff
 .eqv 	WATERBLUE 0x2195f3
+.eqv	BUTTONGRAY 0x9e9e9e
+.eqv	BUTTONRED 0xff1745
+.eqv	BARRIERBROWN 0x4e342e
 	
 .eqv	HEIGHT 64
 .eqv	WIDTH 64
@@ -281,6 +284,12 @@ drawStart:
 	
 	#---------------------------------Draw Score Numbers-------------------------------
 	# Draw Zeroes
+	# Row 1
+	li $t1, WHITE			# $t1 stores the white colour code
+	addi $t4, $zero, 58 		# $t4 stores y-value
+	addi $t2, $zero, 42		# $t2 stores start index
+	addi $t3, $zero, 43		# $t3 stores end index
+	jal drawLine
 	li $t1, WHITE			# $t1 stores the white colour code
 	addi $t4, $zero, 58 		# $t4 stores y-value
 	addi $t2, $zero, 47		# $t2 stores start index
@@ -290,6 +299,12 @@ drawStart:
 	addi $t4, $zero, 58 		# $t4 stores y-value
 	addi $t2, $zero, 52		# $t2 stores start index
 	addi $t3, $zero, 53		# $t3 stores end index
+	jal drawLine
+	# Row 2
+	li $t1, WHITE			# $t1 stores the white colour code
+	addi $t4, $zero, 62 		# $t4 stores y-value
+	addi $t2, $zero, 42		# $t2 stores start index
+	addi $t3, $zero, 43		# $t3 stores end index
 	jal drawLine
 	li $t1, WHITE			# $t1 stores the white colour code
 	addi $t4, $zero, 62 		# $t4 stores y-value
@@ -302,28 +317,122 @@ drawStart:
 	addi $t3, $zero, 53		# $t3 stores end index
 	jal drawLine
 	li $t1, WHITE			# $t1 stores the white colour code
-	# First coloumn
+	# Coloumn 1
+	sw $t1, 15268($t0)		# Paint pixel white
+	sw $t1, 15524($t0)		# Paint pixel white
+	sw $t1, 15780($t0)		# Paint pixel white
+	# Coloumn 2
+	sw $t1, 15280($t0)		# Paint pixel white
+	sw $t1, 15536($t0)		# Paint pixel white
+	sw $t1, 15792($t0)		# Paint pixel white
+	# Coloumn 3
 	sw $t1, 15288($t0)		# Paint pixel white
 	sw $t1, 15544($t0)		# Paint pixel white
 	sw $t1, 15800($t0)		# Paint pixel white
-	# Second coloumn
+	# Coloumn 4
 	sw $t1, 15300($t0)		# Paint pixel white
 	sw $t1, 15556($t0)		# Paint pixel white
 	sw $t1, 15812($t0)		# Paint pixel white
-	# Third coloumn
+	# Coloumn 5
 	sw $t1, 15308($t0)		# Paint pixel white
 	sw $t1, 15564($t0)		# Paint pixel white
 	sw $t1, 15820($t0)		# Paint pixel white
-	# Fourth coloumn
+	# Coloumn 6
 	sw $t1, 15320($t0)		# Paint pixel white
 	sw $t1, 15576($t0)		# Paint pixel white
 	sw $t1, 15832($t0)		# Paint pixel white
 		
+		
+	#---------------------------------Draw Static Platforms----------------------------
+	# Platforms are numbered from bottom to top, left to right
+	# Platform 1 (starting platform)
+	li $t1, BROWN			# $t1 stores the colour code
+	addi $t4, $zero, 49 		# $t4 stores y-value
+	addi $t2, $zero, 0		# $t2 stores start index
+	addi $t3, $zero, 9		# $t3 stores end index
+	jal drawLine
+	# Platform 2
+	li $t1, BROWN			# $t1 stores the colour code
+	addi $t4, $zero, 45 		# $t4 stores y-value
+	addi $t2, $zero, 19		# $t2 stores start index
+	addi $t3, $zero, 27		# $t3 stores end index
+	jal drawLine
+	# Platform 3
+	li $t1, BROWN			# $t1 stores the colour code
+	addi $t4, $zero, 40 		# $t4 stores y-value
+	addi $t2, $zero, 33		# $t2 stores start index
+	addi $t3, $zero, 42		# $t3 stores end index
+	jal drawLine
+	# Platform 4
+	li $t1, BROWN			# $t1 stores the colour code
+	addi $t4, $zero, 40 		# $t4 stores y-value
+	addi $t2, $zero, 60		# $t2 stores start index
+	addi $t3, $zero, 63		# $t3 stores end index
+	jal drawLine
+	# Platform 7
+	li $t1, BROWN			# $t1 stores the colour code
+	addi $t4, $zero, 27 		# $t4 stores y-value
+	addi $t2, $zero, 7		# $t2 stores start index
+	addi $t3, $zero, 15		# $t3 stores end index
+	jal drawLine
+	# Platform 8
+	li $t1, BROWN			# $t1 stores the colour code
+	addi $t4, $zero, 27 		# $t4 stores y-value
+	addi $t2, $zero, 60		# $t2 stores start index
+	addi $t3, $zero, 63		# $t3 stores end index
+	jal drawLine
+	# Platform 9
+	li $t1, BROWN			# $t1 stores the colour code
+	addi $t4, $zero, 21 		# $t4 stores y-value
+	addi $t2, $zero, 0		# $t2 stores start index
+	addi $t3, $zero, 3		# $t3 stores end index
+	jal drawLine
+	# Platform 11
+	li $t1, BROWN			# $t1 stores the colour code
+	addi $t4, $zero, 17 		# $t4 stores y-value
+	addi $t2, $zero, 25		# $t2 stores start index
+	addi $t3, $zero, 35		# $t3 stores end index
+	jal drawLine
+	# Platform 13
+	li $t1, BROWN			# $t1 stores the colour code
+	addi $t4, $zero, 6 		# $t4 stores y-value
+	addi $t2, $zero, 0		# $t2 stores start index
+	addi $t3, $zero, 8		# $t3 stores end index
+	jal drawLine
+	# Platform 14
+	li $t1, BROWN			# $t1 stores the colour code
+	addi $t4, $zero, 6 		# $t4 stores y-value
+	addi $t2, $zero, 60		# $t2 stores start index
+	addi $t3, $zero, 63		# $t3 stores end index
+	jal drawLine
 	
 	
 	
-	
-	
+	#---------------------------------Draw Button--------------------------------------
+	# Button Base
+	li $t1, BUTTONGRAY		# $t1 stores the colour code
+	addi $t4, $zero, 5 		# $t4 stores y-value
+	addi $t2, $zero, 1		# $t2 stores start index
+	addi $t3, $zero, 6		# $t3 stores end index
+	jal drawLine
+	# Button
+	li $t1, BUTTONRED		# $t1 stores the colour code
+	addi $t4, $zero, 4 		# $t4 stores y-value
+	addi $t2, $zero, 2		# $t2 stores start index
+	addi $t3, $zero, 5		# $t3 stores end index
+	jal drawLine
+	# Barrier
+	li $t1, BARRIERBROWN		# $t1 stores the colour code
+	sw $t1, 32($t0)			# Paint colour
+	sw $t1, 288($t0)			# Paint colour
+	sw $t1, 544($t0)			# Paint colour
+	sw $t1, 800($t0)			# Paint colour
+	sw $t1, 1056($t0)		# Paint colour
+	sw $t1, 1312($t0)		# Paint colour
+
+
+
+
 
 	#---------------------------------Terminate Program--------------------------------
 	li $v0, 10			# Assign syscall
@@ -333,19 +442,16 @@ drawStart:
 
 
 
-
-
-
-
+#-----------------------------------------Draw Line Function-------------------------------
 # This function draws a horizontal line given the dimensions
+# let $t0 = base_address
+# let $t1 = colour code
+# let $t2 = i
+# let $t3 = end index 
+# let $t4 = y * width
+# let $t5 = address of pixel
+# let $t6 = address of pixel + base address
 drawLine:
-	# let $t0 = base_address
-	# let $t1 = colour code
-	# let $t2 = i
-	# let $t3 = end index 
-	# let $t4 = y * width
-	# let $t5 = address of pixel
-	# let $t6 = address of pixel + base address
 	sll $t2, $t2, 2			# $t2 = start index * 4
 	sll $t3, $t3, 2			# Set $t3 = end index * 4
 	sll $t4, $t4, 8			# Multiply y value by 256 to get y*width*4	
